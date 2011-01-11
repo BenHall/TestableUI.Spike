16f6a7ec711b0f91f330b92b40a6c37c801bec04
BenHall <ben@benhall.me.uk>
2011-01-11 22:29:37 +0000

    First end-to-end example of serving files

---
+++ TestableUI.Spike/Tests.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Net;
+using FootReST;
+using NUnit.Framework;
+
+namespace TestableUI.Spike.Tests
+{
+  [TestFixture]
+  public class SparkCompilerTests
+  {
+    [Test]
+    public void Can_compile_spark_text()
+    {
+      SparkCompiler compiler = SparkCompiler.Create();
+      string compiledHtml = compiler.Compile(@"CanWeCompile.spark");
+      Assert.IsTrue(compiledHtml.Contains("We have compiled!!"));
+      Assert.IsFalse(compiledHtml.Contains("Something has gone wrong!!"));
+    }
+
+  }
+
+  [TestFixture]
+  public class ServingPagesTests
+  {
+    Server server;
+
+    [SetUp]
+    public void Setup()
+    {
+      SparkCompiler compiler = SparkCompiler.Create();
+      string compiledHtml = compiler.Compile(@"CanWeCompile.spark");
+
+      StartServer(compiledHtml);
+    }
+
+    private void StartServer(string compiledHtml)
+    {
+      server = new Server();
+      server.Start();
+      server.DefineCustomResponse("GET", "/", compiledHtml);
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+      server.Close();
+    }
+
+    [Test]
+    public void Can_we_serve_html_compiled_text()
+    {
+      WebRequest request = WebRequest.Create("http://localhost:5984");
+      string s = new StreamReader(request.GetResponse().GetResponseStream()).ReadToEnd();
+      Assert.IsTrue(s.Contains("We have compiled!!"));
+    }
+  }
+
+}
