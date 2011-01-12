990fb6bda307a73ab3a299fd58101ac6509b3090
BenHall <ben@benhall.me.uk>
2011-01-12 00:12:25 +0000

    Removed OpenRasta.Codec as it didn't seem to be required

--- TestableUI.Spike/SparkCompiler.cs
+++ TestableUI.Spike/SparkCompiler.cs
@@ -1,1 +1,45 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Spark;
+using Spark.FileSystem;
+
+public class SparkCompiler
+{
+  private readonly ISparkViewEngine _engine;
+
+  private SparkCompiler(ISparkViewEngine engine)
+  {
+    _engine = engine;
+  }
+
+  public static SparkCompiler Create()
+  {
+    var settings = new SparkSettings();
+    settings.AddNamespace("System");
+    settings.AddAssembly(Assembly.GetExecutingAssembly());
+    settings.AddViewFolder(ViewFolderType.FileSystem, new Dictionary<string, string> { { "basePath", @"D:\Users\Ben Hall\Documents\Visual Studio 2010\Projects\TestableUI.Spike\TestableUI.Spike\" } });
+    settings.SetPageBaseType(typeof(ViewDataPage));
+
+    var engine = new SparkViewEngine(settings);
+
+    return new SparkCompiler(engine);
+  }
+
+  public string Compile(string page)
+  {
+    var descriptor = new SparkViewDescriptor().AddTemplate(page);
+    var sparkView = _engine.CreateInstance(descriptor);
+
+    StringWriter writer = new StringWriter();
+    sparkView.RenderView(writer);
+
+    return writer.GetStringBuilder().ToString();
+  }
+}
+
+public abstract class ViewDataPage : AbstractSparkView
+{
+  public string model { get; set; }
+}
